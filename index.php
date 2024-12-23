0f089332b07369ada711c21fed815072cb5f9558
migmoresc <migmoresc@outlook.es>
2024-12-23 21:19:45 +0100

    añadido creacion imagens webp

--- index.php
+++ index.php
@@ -1,3 +1,81 @@
+<?php
+function convertToWebp($inputFile, $outputFile)
+{
+    $image = imagecreatefromstring(file_get_contents($inputFile));
+
+    // Check if EXIF extension is available
+    if (function_exists('exif_read_data')) {
+        $exif = @exif_read_data($inputFile);
+
+        if (isset($exif['Orientation'])) {
+            switch ($exif['Orientation']) {
+                case 3: // Rotate 180 degrees
+                    $image = imagerotate($image, 180, 0);
+                    break;
+                case 6: // Rotate 90 degrees clockwise
+                    $image = imagerotate($image, -90, 0);
+                    break;
+                case 8: // Rotate 90 degrees counterclockwise
+                    $image = imagerotate($image, 90, 0);
+                    break;
+            }
+        }
+    }
+    imagewebp($image, $outputFile, 80); // 80 is the quality
+    // imagedestroy($image);
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    header('Content-Type: application/json');
+
+    if (isset($_FILES["files"])) {
+        // Directory to save uploaded files
+        $uploadDir = 'uploads/';
+
+        // Ensure the upload directory exists
+        if (!is_dir($uploadDir)) {
+            mkdir($uploadDir, 0777, true);
+        }
+
+        // Check if files were uploaded
+        if (!empty($_FILES['files']['name'][0])) {
+            $ficheros = [];
+            foreach ($_FILES['files']['name'] as $key => $fileName) {
+                // Create a finfo object to check the actual MIME type of the file
+                $finfo = new finfo(FILEINFO_MIME_TYPE);
+
+                //         // Get the actual MIME type
+                $tipoReal = $finfo->file($_FILES['files']['tmp_name'][$key]);
+                $fileTmpName = $_FILES['files']['tmp_name'][$key];
+                //         // // $fileType = $_FILES['files']['type'][$key];
+                $fileSize = filesize($fileTmpName);
+                $fileError = $_FILES['files']['error'][$key];
+
+                // Allowed file types (jpg and png)
+                $allowedTypes = ['image/jpeg', 'image/png'];
+                // Validate the file
+                if ($fileError === 0 && in_array($tipoReal, $allowedTypes) && $fileSize > 0 && $fileSize < 5242881) {
+                    $safeFileName = preg_replace("/[^a-zA-Z0-9.-_]/", "", $fileName);
+                    $filePath = $uploadDir . basename($safeFileName);
+                    if (move_uploaded_file($fileTmpName, $filePath)) {
+
+                        convertToWebp($filePath, $uploadDir . pathinfo($safeFileName)["filename"] . ".webp");
+                        array_push($ficheros, ['nombre' => $safeFileName, 'url' => $filePath]);
+                    } else {
+                        echo json_encode("Error uploading file '$fileName'.\n");
+                    }
+                } else {
+                    echo json_encode("Invalid file type or error in file '$fileName'.\n");
+                }
+            }
+            echo json_encode($ficheros);
+        } else {
+            echo json_encode("No files uploaded.");
+        }
+    }
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="es">
 
@@ -5,7 +83,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Convert to webp</title>
-    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="./css/reset.css">
     <link id="styles_css" rel="stylesheet" href="./css/styles_light.css">
 </head>
 
@@ -30,21 +108,22 @@
         <div class="containery">
             <div class="entrada_ficheros box">
                 <label for="ficheros" data-translate="ficheros">Selecciona los archivos:</label>
-                <input id="ficheros" type="file" multiple accept=".png,.jpg">
-                <span id="error_ficheros"></span>
+                <input id="ficheros" name="files[]" type="file" multiple accept=".png,.jpg">
+                <span id="error_ficheros" data-translate="error_ficheros"></span>
             </div>
             <div class="listado_ficheros box">
-                <textarea name="listado_ficheros" id="listado_ficheros"></textarea>
+                <div id="div_ficheros">
+
+                </div>
             </div>
             <div class="convertir box">
                 <label for="calidad" data-translate="calidad">Calidad:</label>
-                <input type="number" id="calidad" min="1" max="99">
+                <input type="number" id="calidad" min="1" max="99" value="80" style="text-align: right" ;>
                 <input type="button" id="boton_convertir" value="Convertir" data-translate="convertir">
-                <span id="error_calidad"></span>
+                <span id="error_calidad" data-translate="error_calidad"></span>
             </div>
             <div class="visualizacion box">
-                <img src="" alt="imagen original">
-                <img src="" alt="imagen en webp">
+
             </div>
             <div class="descargar box">
                 <input type="button" data-translate="descargar" value="Descargar">
--- script.js
+++ script.js
@@ -44,7 +44,6 @@ function cambiarBanderas(idioma) {
 }
 
 const toggleTheme = (e) => {
-    console.log(e.target.value);
     let newTheme;
     if (e.target.value == 0) {
         newTheme = "light";
@@ -59,3 +58,60 @@ const toggleTheme = (e) => {
 };
 
 document.getElementById('theme_switch').addEventListener('click', toggleTheme);
+
+document.getElementById("boton_convertir").addEventListener("click", convertir);
+
+function comprobarEntradas() {
+    if (!document.getElementById("ficheros").files.length > 0) {
+        document.getElementById("error_ficheros").style.display = "block";
+        return false;
+    } else {
+        Array.from(document.getElementById("ficheros").files).forEach(e => {
+            if (e.name.split(".").pop() != "png" || e.name.split(".").pop() != "jpg") {
+                return false;
+            }
+        });
+        document.getElementById("error_ficheros").style.display = "none";
+    }
+
+    if (parseInt(document.getElementById("calidad").value) < 1 || parseInt(document.getElementById("calidad").value) > 99) {
+        document.getElementById("error_calidad").style.display = "block";
+        return false;
+    }
+    return true;
+}
+
+function convertir() {
+    const fileInput = document.getElementById('ficheros');
+    const formData = new FormData();
+
+    if (comprobarEntradas()) {
+        // Append all selected files to FormData
+        for (const file of fileInput.files) {
+            formData.append('files[]', file); // 'files[]' allows sending multiple files
+        }
+        // Send the FormData to the server
+        fetch('http://localhost/backend_webp/index.php', {
+            method: 'POST',
+            body: formData
+        }).then(response => response.json())
+            .then(respuesta => {
+                respuesta.forEach(e => {
+                    const a = document.createElement("span");
+                    a.innerText = e["nombre"];
+                    a.addEventListener("click", () => {
+                        mostrarImagen(e["nombre"]);
+                    });
+
+                    document.getElementById("div_ficheros").append(a);
+
+                });
+            })
+            .catch(error => console.error('Error:', error));
+    }
+}
+
+function mostrarImagen(nombre) {
+    console.log(nombre);
+
+}
